c67b67a52c1aa1659371b4a7ec63903256560a87
Antoine de Chassey <antoinedechassey@gmail.com>
2016-11-14 17:57:57 -0500

    Filtres format input

--- gti310/tp3/parser/ConcreteParser.java
+++ gti310/tp3/parser/ConcreteParser.java
@@ -2,6 +2,7 @@ package gti310.tp3.parser;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -13,71 +14,140 @@ import gti310.tp3.entities.RawData;
 public class ConcreteParser implements Parser<RawData> {
 
 	private List<Node<Integer, Integer, Integer>> noeuds = new ArrayList<>();
+	private Integer nbrSommets;
 
 	@Override
-	public RawData parse(String filename) throws IOException {
+	public RawData parse(String filename) {
 		// TODO Auto-generated method stub
 
 		// Open the file
-		FileInputStream fstream = new FileInputStream(filename);
+		FileInputStream fstream = null;
+		try {
+			fstream = new FileInputStream(filename);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			// e1.printStackTrace();
+			System.err.println("The file does not exist.");
+			System.exit(1);
+		}
 		BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
 
 		String strLine;
 		int counterLine = 1;
 
 		// Create a new RawData object
-		Integer nbrSommets = null;
+		nbrSommets = null;
 		Integer valPourInfinie = null;
 		Integer sommetDepart = null;
 		RawData rawData = new RawData(nbrSommets, valPourInfinie, sommetDepart, noeuds);
 
-		// Read the file line by line until "$" is encounterLineed
-		while ((strLine = br.readLine()) != null && !strLine.equals("$")) {
-			if (counterLine == 1) {
-				rawData.setNbrSommets(Integer.parseInt(strLine));
+		// Read the file line by line until "$" is encountered and valid
+		try {
+			while ((strLine = br.readLine()) != null && !strLine.equals("$") && isValid(strLine)) {
+				if (counterLine == 1) {
+					try {
+						rawData.setNbrSommets(Integer.parseInt(strLine));
+						nbrSommets = Integer.parseInt(strLine);
+					} catch (NumberFormatException e) {
+						// e.printStackTrace();
+						System.err.println("Le nombre de sommets est incorrect.");
+						System.exit(1);
+					}
+
+				}
+				if (counterLine == 2) {
+					try {
+						rawData.setValPourInfinie(Integer.parseInt(strLine));
+					} catch (NumberFormatException e) {
+						// e.printStackTrace();
+						System.err.println("La valeur pour l'infinie incorrecte.");
+						System.exit(1);
+					}
+				}
+				if (counterLine == 3) {
+					try {
+						// If no summit, set it to 1
+						if (strLine.equals(""))
+							rawData.setSommetDepart(1);
+						else
+							rawData.setSommetDepart(Integer.parseInt(strLine));
+					} catch (NumberFormatException e) {
+						// e.printStackTrace();
+						System.err.println("Le sommet de départ est incorrect.");
+						System.exit(1);
+					}
+				}
+				if (counterLine > 3) {
+					storeNodes(strLine);
+				}
+				counterLine++;
 			}
-			if (counterLine == 2) {
-				rawData.setValPourInfinie(Integer.parseInt(strLine));
-			}
-			if (counterLine == 3) {
-				// If no summit, set it to 1
-				if (strLine.equals(""))
-					rawData.setSommetDepart(1);
-				else
-					rawData.setSommetDepart(Integer.parseInt(strLine));
-			}
-			if (counterLine > 3) {
-				storeNodes(strLine);
-			}
-			counterLine++;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
 		// Close the input stream
-		br.close();
-
+		try {
+			br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (counterLine < 5) {
+			System.err.println("Le fichier ne contient pas de noeuds.");
+			System.exit(1);
+		}
 		return rawData;
+	}
+
+	private boolean isValid(String strLine) {
+		if (strLine.matches(".*\\d+.*") || strLine.matches("") || strLine.matches(" "))
+			return true;
+		System.err.println("Le fichier n'a pas un bon format.");
+		System.exit(1);
+		return false;
 	}
 
 	/**
 	 * Returns
 	 */
 	private void storeNodes(String strLine) {
-		Node<Integer, Integer, Integer> noeud = new Node<Integer, Integer, Integer>();
+		try {
+			Node<Integer, Integer, Integer> noeud = new Node<Integer, Integer, Integer>();
 
-		// Store the source -- first Integer
-		Integer firstTab = strLine.indexOf("\t");
-		noeud.source = Integer.parseInt(strLine.substring(0, firstTab));
-		// Cut the string
-		strLine = strLine.substring(firstTab + 1);
-		// Store the destination -- second Integer
-		Integer secondTab = strLine.indexOf("\t");
-		noeud.destination = Integer.parseInt(strLine.substring(0, secondTab));
-		// Cut the string
-		strLine = strLine.substring(secondTab + 1);
-		// Store the weight -- third Integer
-		noeud.poids = Integer.parseInt(strLine);
+			// Store the source -- first Integer
+			Integer firstTab = strLine.indexOf("\t");
+			try {
+				noeud.source = Integer.parseInt(strLine.substring(0, firstTab));
+			} catch (Exception e) {
+				System.err.println("Un noeud est incorrect.");
+				System.exit(1);
+			}
+			if ((noeud.source > 0 && noeud.source <= nbrSommets) == false) {
+				System.err.println("La source d'un noeud est incorrecte.");
+				System.exit(1);
+			}
+			// Cut the string
+			strLine = strLine.substring(firstTab + 1);
+			// Store the destination -- second Integer
+			Integer secondTab = strLine.indexOf("\t");
+			noeud.destination = Integer.parseInt(strLine.substring(0, secondTab));
+			if ((noeud.destination > 0 && noeud.destination <= nbrSommets) == false) {
+				System.err.println("La destination d'un noeud est incorrecte.");
+				System.exit(1);
+			}
+			// Cut the string
+			strLine = strLine.substring(secondTab + 1);
+			// Store the weight -- third Integer
+			noeud.poids = Integer.parseInt(strLine);
 
-		noeuds.add(noeud);
+			noeuds.add(noeud);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			// e.printStackTrace();
+			System.err.println("Un noeud est incorrect.");
+			System.exit(1);
+		}
 	}
 
 }
--- gti310/tp3/parser/Parser.java
+++ gti310/tp3/parser/Parser.java
@@ -6,17 +6,18 @@ import java.io.IOException;
  * The Parser interface defines the method through which an input file will be
  * interpreted and an output object will be returned. For greater flexibility,
  * the output object produced from the method is not defined. The inconvenience
- * is that two specialized classes can produce different outputs. The owner
- * will have to know in advance what type of object will be produced by the
- * method when declaring the object : 
+ * is that two specialized classes can produce different outputs. The owner will
+ * have to know in advance what type of object will be produced by the method
+ * when declaring the object :
  * 
- *      public class ConcreteParser implements Parser<MyObject> { ... }
- *      
- * 		Parser<MyObject> parser = new ConcreteParse();
+ * public class ConcreteParser implements Parser<MyObject> { ... }
+ * 
+ * Parser<MyObject> parser = new ConcreteParse();
  * 
  * @author François Caron <francois.caron.7@ens.etsmtl.ca>
  *
- * @param <E> A template that must be redifined by the implementations of the
+ * @param <E>
+ *            A template that must be redifined by the implementations of the
  *            Parser interface.
  */
 public interface Parser<E> {
@@ -27,10 +28,12 @@ public interface Parser<E> {
 	 * file cannot be read, or if the file structure is different than what is
 	 * awaited.
 	 * 
-	 * @param filename The complete path to the file to parse.
-	 * @return An object produced from the input file, or null if something
-	 * 		   went wrong.
-	 * @throws IOException 
+	 * @param filename
+	 *            The complete path to the file to parse.
+	 * @return An object produced from the input file, or null if something went
+	 *         wrong.
+	 * @throws IOException
 	 */
-	E parse(String filename) throws IOException;
+	E parse(String filename);
+
 }
